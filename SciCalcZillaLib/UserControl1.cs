a324d0549513af1a89d72b2af43dd0b614d8563e
Chad Chapman <chad.b.chapman@hotmail.com>
2022-02-17 21:52:08 -0500

    Added comments for event handlers added so far.  Buttons are for main calculator panel and the hexidecimal button panel.

--- SciCalcZillaLib/UserControl1.cs
+++ SciCalcZillaLib/UserControl1.cs
@@ -26,176 +26,351 @@ namespace SciCalcZillaLib
             InitializeComponent();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void UserControl1_Load(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the number "0" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void numberZeroButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the number "1" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void numberOneButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the number "2" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void numberTwoButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the number "3" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void numberThreeButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the number "4" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void numberFourButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the number "5" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void numberFiveButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the number "6" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void numberSixButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the number "7" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void numberSevenButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the number "8" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void numberEightButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the number "9" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void numberNineButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the number "A" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void hexAButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the number "B" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void hexBButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the number "C" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void hexCButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the number "D" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void hexDButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the number "E" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void hexEButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the number "F" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void hexFButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the Addition operation "+" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void additionButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the Subtraction operation "-" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void subtractionButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the Multiplication operation "*" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void multiplicationButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the Division operation "/" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void divisionButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the Equals operation "=" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void equalsButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the Decimal "." button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void decimalButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the Change Sign operation "+/-" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void changSignButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the Square Root operation button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void squareRootButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the Percentage operation "&" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void percentButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the Reciprocal operation "1/x" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void reciprocalButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the MC operation "MC" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void mcButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the MR operation "MR" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void mrButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the MS operation "MS" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void msButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the M Plus operation "M+" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void mPlusButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the M Minus operation "M-" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void mMinusButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the backspace operation "BS" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void backspaceButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the CE operation "CE" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void ceButton_Click(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Event handler for the Clear operation "C" button.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">EventArgs object.</param>
         private void cButton_Click(object sender, EventArgs e)
         {
 
